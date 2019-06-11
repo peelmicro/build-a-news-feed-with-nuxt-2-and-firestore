ec7164219eec3a04faecd9eefbdc17e0cdaf9b0b
Juan Pablo Perez <Juan.Pablo.Perez@retailinmotion.com>
2019-06-11 19:46:19 +0100

    Added nuxt-env to make it work with Heroku

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,10 @@
-export default function({ $axios, app }) {
+export default function({ $axios, app, env }) {
   $axios.onRequest(config => {
+    console.log("env", env);
+    // console.log("context.env.NEWS_API_KEY", context.env.NEWS_API_KEY);
     console.log("process.env.NEWS_API_KEY", process.env.NEWS_API_KEY);
     console.log("app.$env.NEWS_API_KEY", app.$env.NEWS_API_KEY);
     // config.headers.common["Authorization"] = process.env.NEWS_API_KEY;
-    config.headers.common["Authorization"] = app.$env.NEWS_API_KEY;
+    config.headers.common["Authorization"] = env.NEWS_API_KEY;
   });
 }
